cc5b9701d0a7469dd8b1683af20e45b5c403ca91
Axel <latiosaxe@gmail.com>
2017-08-08 20:10:30 -0500

    Nav

--- wp-content/themes/flatsome1.9.9.5/flatsome/header.php
+++ wp-content/themes/flatsome1.9.9.5/flatsome/header.php
@@ -48,29 +48,29 @@ if($flatsome_opt['html_intro'] && is_front_page()) echo '<div class="home-intro"
     <div class="row">
         <div class="large-12 columns">
             <!-- left text -->
-            <div class="left-text left">
-                <div class="html"><?php echo do_shortcode( $flatsome_opt['topbar_left']);?></div><!-- .html -->
-            </div>
-            <!-- right text -->
-            <div class="right-text right">
-                <?php if ( has_nav_menu( 'top_bar_nav' ) ) : ?>
-                <?php
-                wp_nav_menu(array(
-                    'theme_location' => 'top_bar_nav',
-                    'menu_class' => 'top-bar-nav',
-                    'before' => '',
-                    'after' => '',
-                    'link_before' => '',
-                    'link_after' => '',
-                    'depth' => 2,
-                    'fallback_cb' => false,
-                    'walker' => new FlatsomeNavDropdown
-                ));
-                ?>
-                <?php else: ?>
-                Define your top bar navigation in <b>Apperance > Menus</b>
-                <?php endif; ?>
-            </div><!-- .pos-text -->
+<!--            <div class="left-text left">-->
+<!--                <div class="html">--><?php //echo do_shortcode( $flatsome_opt['topbar_left']);?><!--</div><!-- .html -->-->
+<!--            </div>-->
+<!--            <!-- right text -->-->
+<!--            <div class="right-text right">-->
+<!--                --><?php //if ( has_nav_menu( 'top_bar_nav' ) ) : ?>
+<!--                --><?php
+//                wp_nav_menu(array(
+//                    'theme_location' => 'top_bar_nav',
+//                    'menu_class' => 'top-bar-nav',
+//                    'before' => '',
+//                    'after' => '',
+//                    'link_before' => '',
+//                    'link_after' => '',
+//                    'depth' => 2,
+//                    'fallback_cb' => false,
+//                    'walker' => new FlatsomeNavDropdown
+//                ));
+//                ?>
+<!--                --><?php //else: ?>
+<!--                Define your top bar navigation in <b>Apperance > Menus</b>-->
+<!--                --><?php //endif; ?>
+<!--            </div><!-- .pos-text -->-->
 
         </div><!-- .large-12 columns -->
     </div><!-- .row -->
@@ -300,7 +300,7 @@ if($flatsome_opt['html_intro'] && is_front_page()) echo '<div class="home-intro"
                     </a>
                     <div class="sub-menu">
                         <div>
-                            <div class="sub-menu-left sub-menu-sec"> 
+                            <div class="sub-menu-left sub-menu-sec">
                                 <?php wp_nav_menu( array('menu' => 'bala' )); ?>
 <!--                                <div class="sub-menu-left-temp-1"></div>-->
                             </div>
